dabdc70eed8e815718385f8078420b3768754828
Felobateer <felobateersamy01@gmail.com>
2023-12-15 14:37:51 +0100

    register design complete

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.less'
+  styleUrl: './register.component.less',
 })
 export class RegisterComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(8)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  handleLogin() {
+    console.log(this.form);
+  }
 }
