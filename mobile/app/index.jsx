b8ff79a6e7ef26e17a8e2a48b0b62045a34e8715
Felobateer <felobateersamy01@gmail.com>
2024-05-10 17:51:33 +0200

    setup done

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,10 +1,16 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View className="flex-1 items-center justify-center bg-slate-600">
+      <Text className="text-5xl">index</Text>
+      <StatusBar style="auto" />
+      <Link href="/test" style={{ fontSize: 20, color: "red" }}>
+        Go to test
+      </Link>
     </View>
   );
 };
