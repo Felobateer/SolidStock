4d70f5f440aa1c49fdecfbab00a551f6cf8e8b8c
Felobateer <felobateersamy01@gmail.com>
2024-05-14 08:52:11 +0200

    user register

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,20 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { Stack } from "expo-router";
+import index from ".";
+
+const logo = require("../assets/solidstockLogo.png");
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <View className="bg-slate-700 flex-1">
+      <StatusBar style="auto" />
+      <View className="w-full flex justify-center">
+        <Image className="mx-auto my-[15vh]" source={logo} />
+      </View>
+      <View className="w-4/5 h-[50vh] mx-auto fill-slate-700">
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="register" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="forgetPassword"
+            options={{ headerShown: false }}
+          />
+        </Stack>
+      </View>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
