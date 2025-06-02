d560e537efc89679489ea648effa040a2511047a
Felobateer <felobateersamy01@gmail.com>
2025-06-02 17:28:00 +0200

    update

--- frontend/src/app/components/table/table.component.ts
+++ frontend/src/app/components/table/table.component.ts
@@ -20,7 +20,7 @@ import { CommonModule } from '@angular/common';
 export class TableComponent {
   theads: string[] = [
     '',
-    'Industry',
+    // 'Industry',
     'Symbol',
     'Name',
     'Buy',
--- frontend/src/app/services/enviroment.ts
+++ frontend/src/app/services/enviroment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/services/',
+  apiUrl: 'https://solidstock.onrender.com/services/',
 };
